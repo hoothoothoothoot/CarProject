89b6ef1e5d8b225670ca9e62b971f5ebcfcd6f4e
Unknown <weslie.rabeson@gmail.com>
2017-11-11 14:15:09 +0100

    controller update/view update

--- controller/ControllerVoiture.php
+++ controller/ControllerVoiture.php
@@ -7,7 +7,9 @@ require_once ("$tab"); // chargement du modèle
 class ControllerVoiture {
     public static function readAll() {
         $tab_v = ModelVoiture::getAllVoitures();     //appel au modèle pour gerer la BD
-        $path_to_list = array("view", "voiture", "list.php");
+        $controller = 'voiture';
+        $view = 'list';
+        $path_to_list = array("view", "view.php");
         $tab_to_list = File::build_path($path_to_list);
         require ("$tab_to_list");  //redirige vers la vue
     }
@@ -15,16 +17,20 @@ class ControllerVoiture {
     public static function read() {
         $immat = $_GET['immat'];
         $v = ModelVoiture::getVoitureByImmat($immat);
-        $tab_to_detail = File::build_path(array("view","voiture","detail.php"));
+        $controller = 'voiture';
+        $view = 'detail';
+        $tab_to_detail = File::build_path(array("view","view.php"));
         if ($v == true) {
             require ("$tab_to_detail");
         } else {
-            require (File::build_path(array("view","voiture","error.php")));
+            require (File::build_path(array("view","view.php")));
         }
     }
     
     public static function create() {
-        require (File::build_path(array("view","voiture","create.php")));
+        $controller = 'voiture';
+        $view = 'create';
+        require (File::build_path(array("view","view.php")));
     }
     
     public static function created() {
